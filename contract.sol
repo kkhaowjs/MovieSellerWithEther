3d2deb5e64ac497e061e296a611bfb09c27701e9
kkhaowjs <jetsadin01@gmail.com>
2024-02-15 16:53:00 +0700

    add contract

---
+++ contract.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+contract ProofOfAC_Movie {  
+
+  event Add(
+    address owner,   
+    string movie
+  );
+  
+  function purchase(string memory name) public payable {
+    emit Add(msg.sender, name);
+  }
+}
